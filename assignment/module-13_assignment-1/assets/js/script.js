20850238d4f73457f96b1ebb84a53265d5fcd974
mzhratul <mdzahid.hr@gmail.com>
2023-06-23 20:07:49 +0600

    Hello

--- assignment/module-13_assignment-1/assets/js/script.js
+++ assignment/module-13_assignment-1/assets/js/script.js
@@ -1,27 +1,66 @@
-$(function () {
-    "use strict";
-})(
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-    () => {
-        "use strict";
+window.addEventListener("load", () => {
+    document.getElementById("preloader").style='display:none;opacity:0;visibility:hidden;transition:.5s;';
+});
 
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.querySelectorAll(".needs-validation");
-
-        // Loop over them and prevent submission
-        Array.from(forms).forEach((form) => {
-            form.addEventListener(
-                "submit",
-                (event) => {
-                    if (!form.checkValidity()) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-
-                    form.classList.add("was-validated");
-                },
-                false
-            );
-        });
+if(localStorage.getItem('mode') == 'dark'){
+    document.getElementsByTagName('html')[0].setAttribute("data-bs-theme", 'dark');
+    document.getElementById('modeChanger').innerText = 'Light Mode'
+}
+document.getElementById('modeChanger').addEventListener("click", (e)=>{
+    let themeMode = document.getElementsByTagName('html')[0].getAttribute("data-bs-theme")
+    if(themeMode == 'dark'){
+        document.getElementsByTagName('html')[0].setAttribute("data-bs-theme", 'light')
+        e.target.innerText = 'Dark Mode';
+        localStorage.setItem('mode', 'light')
+    }else{
+        document.getElementsByTagName('html')[0].setAttribute("data-bs-theme", 'dark')
+        e.target.innerText = 'Light Mode';
+        localStorage.setItem('mode', 'dark')
     }
-)();
+})
+
+
+const ctx = document.getElementById("reactVsVue");
+
+new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: ["React", "Vue"],
+        datasets: [
+            {
+                label: "Download",
+                data: [82882457, 388967690],
+                backgroundColor:'orange',
+                borderColor:'black',
+                borderWidth: 1,
+            },
+            {
+                label: "Performance",
+                data: [8, 10],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: ["rgb(255, 99, 132)", "rgb(255, 159, 64)"],
+                borderWidth: 1,
+            },
+            {
+                label: "Eco-system",
+                data: [10, 8],
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: ["rgb(255, 99, 132)", "rgb(255, 159, 64)"],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+});
